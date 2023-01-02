2476f5f1eab9761ca1093f938cf9affbe7ed6e46
blessingumzy <blessingumera25@gmail.com>
2023-01-02 10:02:08 -0800

    instructions

--- 0x07-pointers_arrays_strings/4-main.c
+++ 0x07-pointers_arrays_strings/4-main.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-    char *s = "hello, world";
-    char *f = "world";
-    char *t;
-
-    t = _strpbrk(s, f);
-    printf("%s\n", t);
-    return (0);
+char *s = "hello, world";
+char *f = "world";
+char *t;
+t = _strpbrk(s, f);
+printf("%s\n", t);
+return (0);
 }
